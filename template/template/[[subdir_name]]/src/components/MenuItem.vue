ce5312b0caf79f6a93f2787a4c903b6c0fedc1a8
worldworm <13227454+worldworm@users.noreply.github.com>
2024-07-09 23:58:07 +0200

    removes example drawer

---
+++ template/template/[[subdir_name]]/src/components/MenuItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-item
+    clickable
+    tag="a"
+    :to="link"
+    :href="externalLink"
+    v-ripple
+    :active="$router.currentRoute.value.path == link"
+  >
+    <q-item-section v-if="icon" avatar>
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+    </q-item-section>
+  </q-item>
+  <q-separator v-if="separator" />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+defineOptions({
+  name: "MenuItem",
+});
+
+export interface MenuItemProps {
+  title: string;
+  link?: string;
+  icon?: string;
+  separator?: boolean;
+}
+
+const props = withDefaults(defineProps<MenuItemProps>(), {
+  link: "#",
+  icon: "",
+  separator: false,
+});
+
+const externalLink = computed((): string | undefined => {
+  return /^https?:\/\//.test(props.link) ? props.link : undefined;
+});
+</script>
--- template/template/[[subdir_name]]/src/layouts/MainLayout.vue
+++ template/template/[[subdir_name]]/src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
         <q-btn
@@ -11,31 +11,17 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-toolbar-title> Quasar App </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+        <q-item-label header> Menu </q-item-label>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
+        <MenuItem v-for="link in linksList" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
@@ -47,60 +33,24 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import EssentialLink, { EssentialLinkProps } from "components/EssentialLink.vue";
+import MenuItem, { MenuItemProps } from "components/MenuItem.vue";
 
 defineOptions({
-  name: "MainLayout"
+  name: "MainLayout",
 });
 
-const linksList: EssentialLinkProps[] = [
+const linksList: MenuItemProps[] = [
   {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev"
+    title: "Dashboard",
+    icon: "dashboard",
+    link: "/",
+    separator: true,
   },
-  {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework"
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev"
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev"
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev"
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev"
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev"
-  }
 ];
 
 const leftDrawerOpen = ref(false);
 
-function toggleLeftDrawer () {
+function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 </script>
